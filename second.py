4e878184f1c1764bb0f667dd5d59f1aa0a82abbe
swetha-1398 <58969136+swetha-1398@users.noreply.github.com>
2019-12-16 23:36:51 -0800

    Add files via upload

---
+++ second.py
@@ -0,0 +1,15 @@
+a=int(input(5))
+
+b=int(input(5))
+
+if (a>b):
+
+   print (a)
+
+else:
+
+   print (b)
+
+elif:
+
+   print (both are equal)
---
+++ third.py
@@ -0,0 +1,15 @@
+a=int(input())
+
+b=int(input())
+
+if(a>b):
+
+   print(a)
+
+elif(b>a):
+
+   print(b)
+
+else:
+
+   print("both are equal")
